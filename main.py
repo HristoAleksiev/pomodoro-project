4858e6cc7de172a728841b78aadb7dc35fbc9195
Алексиев <kadoll555@gmail.com>
2021-05-09 14:59:45 +0300

    added reset functionality and finishing touches

--- main.py
+++ main.py
@@ -7,12 +7,28 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 1
-SHORT_BREAK_MIN = 0.5
-LONG_BREAK_MIN = 5
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
 cycle_count = 0
+timer = None
+checkmarks = ""
 
 # ---------------------------- TIMER RESET ------------------------------- # 
+
+
+def reset_tool():
+    global cycle_count, checkmarks
+
+    cycle_count = 0
+    checkmarks = ""
+
+    window.after_cancel(timer)
+
+    title.config(text="Timer", fg=GREEN)
+    checkmark.config(text=checkmarks)
+    canvas.itemconfig(timer_display, text="00:00")
+
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
@@ -39,6 +55,7 @@ def start_timer():
 
 
 def timer_count(count):
+    global timer, checkmarks
     minutes_to_count = math.floor(count / 60)
     second_to_count = count % 60
 
@@ -47,12 +64,18 @@ def timer_count(count):
 
     canvas.itemconfig(timer_display, text=f"{minutes_to_count}:{second_to_count}")
     if count > 0:
-        window.after(200, timer_count, count - 1)
+        timer = window.after(1000, timer_count, count - 1)
     else:
         start_timer()
+        work_sessions_completed = math.floor(cycle_count / 2)
 
+        for _ in range(work_sessions_completed):
+            checkmarks += "✓"
+
+        checkmark.config(text=checkmarks)
 
 # ---------------------------- UI SETUP ------------------------------- #
+
 
 window = Tk()
 window.title("Pomodoro Timer")
@@ -71,15 +94,15 @@ title.grid(column=1, row=0)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_image = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_image)
-timer_display = canvas.create_text(100, 130, text=0, fill="white", font=(FONT_NAME, 35, "bold"))
+timer_display = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
 
-checkmark = Label(text="✓", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 15, "bold"))
+checkmark = Label(bg=YELLOW, fg=GREEN, font=(FONT_NAME, 15, "bold"))
 checkmark.grid(column=1, row=3)
 
 # Column 3
 
-reset = Button(text="Reset", highlightthickness=0)
+reset = Button(text="Reset", highlightthickness=0, command=reset_tool)
 reset.grid(column=2, row=2)
 
 window.mainloop()
